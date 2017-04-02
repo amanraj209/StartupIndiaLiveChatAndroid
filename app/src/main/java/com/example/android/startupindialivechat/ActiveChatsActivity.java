b521500d7eb2e66242d7673445c4018317c8dbe8
amanraj209 <amanraj20996@gmail.com>
2017-04-02 10:29:36 +0530

    authentication added

---
+++ app/src/main/java/com/example/android/startupindialivechat/ActiveChatsActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.startupindialivechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActiveChatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active_chats);
+    }
+}
--- app/src/main/java/com/example/android/startupindialivechat/Authentication.java
+++ app/src/main/java/com/example/android/startupindialivechat/Authentication.java
@@ -1,10 +1,15 @@
 package com.example.android.startupindialivechat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -21,12 +26,16 @@ public class Authentication extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+    }
 
+    public void signInOfficial(View view) {
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
 
         if (firebaseUser != null) {
-            Log.i("user", firebaseUser.toString());
+            startActivity(new Intent(this, ChatsActivity.class));
+            finish();
+            return;
         }
         else {
             startActivityForResult(AuthUI.getInstance()
@@ -37,5 +46,41 @@ public class Authentication extends AppCompatActivity {
                     RC_SIGN_IN
             );
         }
+    }
+
+    public void continueUser(View view) {
+        startActivity(new Intent(this, UserActivity.class));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == ResultCodes.OK) {
+                startActivity(new Intent(this, ChatsActivity.class));
+                finish();
+                return;
+            }
+            else {
+                if (response == null) {
+                    return;
+                }
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackbar(R.string.unknown_error);
+                    return;
+                }
+            }
+            showSnackbar(R.string.unknown_sign_in_response);
+        }
+    }
+
+    private void showSnackbar(int response) {
+        Snackbar.make(findViewById(R.id.activity_authentication), getString(response), Snackbar.LENGTH_SHORT).show();
     }
 }
